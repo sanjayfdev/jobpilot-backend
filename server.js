87dbf69bb1f89d9734c90f1bee618da2b6c554cb
sanjayfdev <sanjaykumar15.dev@gmail.com>
2026-02-01 10:28:40 +0530

    add frontend origin in cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors('*'));
+app.use(cors({origin: 'https://jobpilot-frontend.vercel.app'}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
