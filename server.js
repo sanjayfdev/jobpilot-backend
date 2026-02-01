0555c73c6eb6fff2db21a43fcda12e568c1efc42
sanjayfdev <sanjaykumar15.dev@gmail.com>
2026-02-01 15:09:04 +0530

    fixed create-job endpoint

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: 'https://jobpilot-frontend.vercel.app'}));
+app.use(cors({ origin: ['https://jobpilot-frontend.vercel.app', 'http://localhost:5173'], credentials: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
